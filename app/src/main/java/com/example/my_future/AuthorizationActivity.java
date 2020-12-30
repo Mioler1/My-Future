0b236bbacc24c1e6bba1b83bf1381d67b8073fd8
SergeyGaldin <veretennik-v@mail.ru>
2020-12-30 21:03:19 +0300

    registr

--- app/src/main/java/com/example/my_future/AuthorizationActivity.java
+++ app/src/main/java/com/example/my_future/AuthorizationActivity.java
@@ -10,6 +10,14 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.example.my_future.Models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,6 +28,10 @@ public class AuthorizationActivity extends AppCompatActivity {
     EditText email, password, repeatPassword;
     Button come, registration;
 
+    FirebaseDatabase db;
+    DatabaseReference myRef;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +41,10 @@ public class AuthorizationActivity extends AppCompatActivity {
     }
 
     private void init() {
+        db = FirebaseDatabase.getInstance();
+        myRef = db.getReference("Users");
+        mAuth = FirebaseAuth.getInstance();
+
         come = findViewById(R.id.comeButton);
         registration = findViewById(R.id.regButton);
 
@@ -53,24 +69,39 @@ public class AuthorizationActivity extends AppCompatActivity {
     }
 
     private void Registration() {
-        email = findViewById(R.id.email_authorization);
-        password = findViewById(R.id.password_authorization);
+        email = findViewById(R.id.email_registration);
+        password = findViewById(R.id.password_registration);
         repeatPassword = findViewById(R.id.repeat_password_registration);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference();
+        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        User user = new User();
+                        user.setEmail(email.getText().toString());
+                        user.setPassword(password.getText().toString());
 
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                MyToast("Успешно");
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                MyToast("Провал");
-            }
-        });
+                        myRef.child(mAuth.getUid()).setValue(user)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        MyToast("Авторизация завершена");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        MyToast("Авторизация провалена");
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        MyToast("Пользователь уже существует");
+                    }
+                });
     }
 
     public void onClickAuthorization(View view) {
