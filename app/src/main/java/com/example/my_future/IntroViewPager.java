3296ac5e74c735d1290ebefa1f2bc2eec09c1726
Mioler1 <graveer3759@mail.ru>
2021-01-12 20:03:53 +0300

    v.0.0.1

---
+++ app/src/main/java/com/example/my_future/IntroViewPager.java
@@ -0,0 +1,64 @@
+package com.example.my_future;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class IntroViewPager extends PagerAdapter {
+
+
+    Context mContext;
+    List<ScreenItemLayout> mListScreen;
+
+    public IntroViewPager(Context mContext, List<ScreenItemLayout> mListScreen) {
+        this.mContext = mContext;
+        this.mListScreen = mListScreen;
+    }
+
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layoutScreenIntro = inflater.inflate(R.layout.layout_screen_intro, null);
+
+        ImageView ScreenImg = layoutScreenIntro.findViewById(R.id.imageIntro);
+        TextView title = layoutScreenIntro.findViewById(R.id.intro_title);
+        TextView description = layoutScreenIntro.findViewById(R.id.intro_description);
+
+        title.setText(mListScreen.get(position).getTitle());
+        description.setText(mListScreen.get(position).getDescription());
+        ScreenImg.setImageResource(mListScreen.get(position).getScreenImg());
+
+        container.addView(layoutScreenIntro);
+
+        return layoutScreenIntro;
+
+    }
+
+    @Override
+    public int getCount() {
+        return mListScreen.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+
+        container.removeView((View)object);
+
+    }
+}
